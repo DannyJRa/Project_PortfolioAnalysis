b7b9db5f439ca9ad6d2c44c7f0d25836de433be7
DannyJRa <danny_zurich@outlook.com>
2017-08-27 11:01:24 +0200

    deleted tabs

--- app.R
+++ app.R
@@ -41,11 +41,9 @@ ui <- fluidPage(
 # Main Panel               
                 mainPanel(
                         tabsetPanel(type="tab",
-                                    tabPanel("Plot",plotOutput("plot")),
-                                    tabPanel("summary",dataTableOutput("table")),
-                                    tabPanel("close",dataTableOutput("table1")),
-                                    tabPanel("CAPM",dataTableOutput("tableCAPM")),
-                                    tabPanel("Wealth",plotlyOutput("plot2")))
+                                    tabPanel("Wealth",plotlyOutput("plot2"))
+                                 
+                                  )
                     
                 )
         
@@ -56,144 +54,6 @@ ui <- fluidPage(
 server <- function(input, output) {
         
       
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        stockData <- new.env()
-        dataInput <- reactive({ 
-                if(input$go==0){return()} #confirming button click
-                isolate({
-                        input$go
-                        getSymbols(input$txt, src = "yahoo", env=stockData,from =as.Date(input$dates) )    
-                        Data <- data.frame()
-                        
-                        validate(need(input$txt != "", label = "stock"))
-                        for (i in 1:length(input$txt)) {
-                                x[[i]] <- get(input$txt[i], pos=stockData)  # get data from stockData environment  
-                                Data <- cbind(Data,diff(log(Cl(x[[i]]))))
-                        }
-                        Data
-                })      
-        })                 
-        dataCAPM <- reactive({ 
-                                if(input$go==0){return()} #confirming button click
-                                isolate({
-                                        input$go
-                                        tickerSelected=input$txt
-                                        
-                                        
-                                        Ra <- tickerSelected %>%
-                                                tq_get(get  = "stock.prices",
-                                                       from = "2010-01-01",
-                                                       to   = "2015-12-31") %>%
-                                                group_by(symbol) %>%
-                                                tq_transmute(select     = adjusted, 
-                                                             mutate_fun = periodReturn, 
-                                                             period     = "monthly", 
-                                                             col_rename = "Ra")
-                                        Ra
-                                        
-                                        
-                                        # Set name for BRK-A to BRK.A
-                                        setSymbolLookup(GDAXI = list(name = "^GDAXI"))
-                                        
-                                        
-                                        # Load BRK.A and ATT data
-                                        getSymbols(c("GDAXI"))
-                                        
-                                        Rb <- "GDAXI" %>%
-                                                tq_get(get  = "stock.prices",
-                                                       from = "2010-01-01",
-                                                       to   = "2015-12-31") %>%
-                                                tq_transmute(select     = adjusted, 
-                                                             mutate_fun = periodReturn, 
-                                                             period     = "monthly", 
-                                                             col_rename = "Rb")
-                                        Rb
-                                        
-                                        RaRb <- left_join(Ra, Rb, by = c("date" = "date"))
-                                        RaRb
-                                        
-                                        RaRb_capm <- RaRb %>%
-                                                tq_performance(Ra = Ra, 
-                                                               Rb = Rb, 
-                                                               performance_fun = table.CAPM)
-                                        RaRb_capm
-                                        })
-                                                               
-        })
-        
-                       
-                        
-           dataWealth <- reactive({ 
-                                        if(input$go==0){return()} #confirming button click
-                                        isolate({
-                                                input$go                                
-                                        
-                                
-                                        wts_map <- tibble(
-                                        symbols = c(ticker[1:2]),
-                                        weights = c(0.5, 0.5)
-                                )
-                                wts_map
-                                
-                                
-                                
-                                portfolio_returns_monthly <- stock_returns_monthly %>%
-                                        tq_portfolio(assets_col  = symbol, 
-                                                     returns_col = Ra, 
-                                                     weights     = wts_map, 
-                                                     col_rename  = "Ra")            
-                        
-                        
-                                portfolio_growth_monthly <- stock_returns_monthly %>%
-                                        tq_portfolio(assets_col   = symbol, 
-                                                     returns_col  = Ra, 
-                                                     weights      = wts_map, 
-                                                     col_rename   = "investment.growth",
-                                                     wealth.index = TRUE) %>%
-                                        mutate(investment.growth = investment.growth * 10000)
-                                portfolio_growth_monthly
-                                        })
-           })                      
-       
-        
-        Last_Close <- reactive({
-                if(input$go==0){return()} #confirming button click
-                isolate({
-                        input$go
-                        validate(need(input$txt != "", label = "stock"))
-                        for (i in 1:length(input$txt)) {
-                                x[[i]] <- get(input$txt[i], pos=stockData)  # get data from stockData environment  
-                                clo <- cbind(clo,Cl(x[[i]]))
-                        }
-                        clo
-                })
-        })
-        
         output$plot <- renderPlot(chart.Correlation(dataInput()))
         output$table <- DT::renderDataTable(datatable(as.data.frame(Cl(dataInput()))))
         output$table1 <- DT::renderDataTable(datatable(as.data.frame(Cl(Last_Close()))))
